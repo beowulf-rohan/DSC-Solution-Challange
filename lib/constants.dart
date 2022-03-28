c69816652ffa4920ff6ca5c37e8c4d808a7a47e3
Rishav Raj <rishav@Rishavs-Air.mshome.net>
2022-03-28 22:49:54 +0530

    constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,31 +4,17 @@ const kPrimaryColor = Color(0xFF488fb1);
 const buttonHeight = 45.0;
 
 const String USER_COLLECTION = "UserDatabase";
-const String PROPERTY_COLLECTION = "PropertyDatabase";
-const String FAQ_COLLECTION = "FaqDatabase";
-const String TERMS_AND_CONDITIONS_COLLECTION = "TnCDatabase";
 
 const String STUDENT_EMAIL = "Email";
 const String STUDENT_NAME = "Name";
 const String STUDENT_CONTACT = "Contact";
 const String STUDENT_ROLL_NO = "Roll No";
 const String STUDENT_DEPARTMENT = "Departnent";
+const String STUDENT_UID = "S_UID";
 
-const String PROPERTY_NAME = "Name";
-const String PROPERTY_CONTACT = "Contact";
-const String PROPERTY_ADDRESS = "Address";
-const String PROPERTY_COUNTRY = "Country";
-const String PROPERTY_STATE = "State";
-const String PROPERTY_CITY = "City";
-const String PROPERTY_PINCODE = "Pin-Code";
-const String PROPERTY_GOOGLE_LOCATION = "Google-Location";
-const String PROPERTY_SERVICE_PROVIDER = "Service-Provider";
-const String PROPERTY_CONSEUMER_NUMBER = "Consumer-Number";
-const String PROPERTY_VERIFIED = "Is-Verified";
-const String PROPERTY_LIST = "Property-List";
-
-const String FAQ_QUESTION = "Question";
-const String FAQ_ANSWER = "Answer";
-
-const String TERMS_AND_CONDITIONS_STATEMENT = "Statement";
-const String TERMS_AND_CONDITIONS_EXPLANATION = "Explanation";
+const String TEACHER_EMAIL = "Email";
+const String TEACHER_NAME = "Name";
+const String TEACHER_CONTACT = "Contact";
+const String TEACHER_ROLL_NO = "Roll No";
+const String TEACHER_DEPARTMENT = "Departnent";
+const String TEACHER_UID = "T_UID";
